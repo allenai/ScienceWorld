fb3b21546bb72f4ccff3cdd57d0d51059cb3157f
PeterAJansen <pajansen@email.arizona.edu>
2021-10-29 14:11:25 -0700

    refactored action: use device

--- src/main/scala/scienceworld/actions/ActionUseDevice.scala
+++ src/main/scala/scienceworld/actions/ActionUseDevice.scala
@@ -3,6 +3,7 @@ package scienceworld.actions
 import language.model.{ActionExprIdentifier, ActionExprOR, ActionRequestDef, ActionTrigger}
 import scienceworld.input.ActionDefinitions.mkActionRequest
 import scienceworld.input.{ActionDefinitions, ActionHandler}
+import scienceworld.objects.agent.Agent
 import scienceworld.objects.devices.Device
 import scienceworld.struct.EnvObject
 
@@ -12,23 +13,41 @@ import scienceworld.struct.EnvObject
 class ActionUseDevice(action:ActionRequestDef, assignments:Map[String, EnvObject]) extends Action(action, assignments) {
 
   override def isValidAction(): (String, Boolean) = {
-    // Unimplemented
-    return ("", true)
-  }
-
-  override def runAction(): String = {
     val agent = assignments("agent")
     val device = assignments("device")
     val patientObj = assignments("patient")
 
-    if ((device.propDevice.isDefined) && (device.propDevice.get.isUsable)) {
-      val (success, resultStr) = device.useWith(patientObj)
-      if (!success) return "I'm not sure how to use those two things together."
-
-      return resultStr
+    // Check 1: Check that agent is valid
+    agent match {
+      case a:Agent => { }
+      case _ => return ("I'm not sure what that means", false)
     }
 
-    return "I'm not sure how to use those two things together."
+    // Check 2: Check that the device is a device
+    if ((!device.propDevice.isDefined) || (!device.propDevice.get.isUsable)) {
+      return ("I'm not sure how to use the " + device.name + ". ", false)
+    }
+
+    //TODO: Check 3: Check that using the device on that thing is valid?
+
+    // Checks complete -- if we reach here, the action is valid
+    return ("", true)
+  }
+
+  override def runAction(): (String, Boolean) = {
+    val agent = assignments("agent")
+    val device = assignments("device")
+    val patientObj = assignments("patient")
+
+    // Do checks for valid action
+    val (invalidStr, isValid) = this.isValidAction()
+    if (!isValid) return (invalidStr, false)
+
+    // Use the device on the patient object
+    val (success, resultStr) = device.useWith(patientObj)
+    if (!success) return ("I'm not sure how to use those two things together.", false)      //## TODO: Elegant way of handling this with checks?
+
+    return (resultStr, true)
   }
 
 }
