b9bccba149f0101f1f04b5f12da376341fdd06d8
Ruoyao Wang <wsxzwps43@126.com>
2021-10-30 23:25:46 -0700

    update training script

---
+++ agents/gpt2/train/train_gpt2_agent.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+export DATAIN=../../../python-api/saveout.txt
+export OUTPUT_DIR=/tmp/test-clm
+
+
+python run_clm.py \
+    --model_name_or_path /data/ai2-mosaic-public/comet-distill \
+    --tokenizer_name /data/ai2-mosaic-public/comet-distill-tokenizer \
+    --train_file ${DATAIN} \
+    --validation_file ${DATAIN} \
+    --do_train \
+    --do_eval \
+    --output_dir ${OUTPUT_DIR} \
+	--num_train_epochs 10 \
+	--per_device_train_batch_size 1 \
+	--per_device_eval_batch_size 1 \
+	--overwrite_output_dir
+
+
