975adb5a578af887c3923f39e6d1b4811f1eabc2
ruoyao <wsxzwps43@126.com>
2021-11-19 18:19:41 -0700

    use gpt2 small model

--- agents/gpt2/train/train_gpt2_on_atomic.sh
+++ agents/gpt2/train/train_gpt2_on_atomic.sh
@@ -3,7 +3,7 @@
 #--model_name_or_path gpt2-medium \
 export TRAIN=atomic_train.json
 export VAL=atomic_val.json
-export MODELNAME=gpt2-large
+export MODELNAME=gpt2-small
 export TOKENIZERPATH=/home/ruoyao/comet-distill-tokenizer
 
 deepspeed --num_gpus=4 run_clm.py \
@@ -18,7 +18,9 @@ deepspeed --num_gpus=4 run_clm.py \
         --dataset_config_name wikitext-2-raw-v1 \
         --per_device_train_batch_size 1 \
         --per_device_eval_batch_size 1 \
-        --output_dir /home/ruoyao/output_dump/test-clm \
-        --overwrite_output_dir 
+        --output_dir /home/ruoyao/output_dump/gpt2-small-automic \
+        --overwrite_output_dir\
+        --max_train_samples 1 \
+
 
 #        --fp16 \
