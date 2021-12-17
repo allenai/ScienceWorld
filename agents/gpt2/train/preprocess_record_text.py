fbf2469bcdd226fd08e08b1d0be7abd32370dc45
Ruoyao Wang <wsxzwps43@126.com>
2021-12-16 19:46:30 -0700

    update recording data preprocessing; run_clm can work with non language model labels

---
+++ agents/gpt2/train/preprocess_record_text.py
@@ -0,0 +1,23 @@
+import json
+import os
+
+record_dir = "/home/ruoyao/Documents/projects/virtualenv-scala2/python-api/recordings"
+
+output = []
+
+for filename in os.listdir(record_dir):
+    with open(os.path.join(record_dir, filename)) as f:
+        data = json.load(f)
+    
+    for i in range(len(data)):
+        if i == 0:
+            output_text = f"[CLS] {data[i]['taslDescription']} [SEP] [SEP] {data[i]['observation']} [SEP]"
+        else:
+            output_text = f"[CLS] {data[i-1]['observation']} [SEP] {data[i-1]['userInput']} [SEP] {data[i]['observation']} [SEP]"
+        label = data[i]["userInput"]
+        output.append({"text": output_text, "labels": label})
+
+with open("record_data.json", "w") as f:
+    for data in output:
+        json.dump(data, f)
+        f.write('\n')
