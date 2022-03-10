c75cb4747bc2cca0c8cd06666c6432ba45c3d2ec
PeterAJansen <pajansen@email.arizona.edu>
2022-03-09 18:24:13 -0700

    More cleanup in preparation for first release

--- agents/gpt2/eval/run-gpt2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export DATAIN=saveout.txt
-export MODEL_DIR=/tmp/test-clm
-#export MODEL_DIR=gpt2-medium
-
-
-python run_generation.py \
-    --model_type=gpt2 \
-    --model_name_or_path=${MODEL_DIR}
