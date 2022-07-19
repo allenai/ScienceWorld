989c76db45055cee29efce1d686da064a93ac5ab
Alex Hedges <aphedges1@gmail.com>
2022-07-19 12:18:35 -0400

    Fix scienceworld-scala version

--- simulator/package.sh
+++ simulator/package.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sbt assembly
-cp target/scala-2.12/scienceworld-scala-assembly-1.0.0.jar ../scienceworld/scienceworld-1.0.0.jar
+cp target/scala-2.12/scienceworld-scala-assembly-1.0.2.jar ../scienceworld/scienceworld-1.0.2.jar
