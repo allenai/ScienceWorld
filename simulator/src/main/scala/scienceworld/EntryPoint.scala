a4a5b34b349f6848a1297384c48f9f90a8ae3b06
PeterAJansen <pajansen@email.arizona.edu>
2022-07-21 10:35:02 -0700

    minor changes

--- simulator/src/main/scala/scienceworld/EntryPoint.scala
+++ simulator/src/main/scala/scienceworld/EntryPoint.scala
@@ -128,9 +128,7 @@ object EntryPoint {
 
         println("Possible objects: " + interface.agentInterface.get.getPossibleObjects().mkString(", "))
 
-        val agent = interface.agentInterface.get.agent
-        val accessibleObjects = InputParser.collectAccessibleObjects(objectTreeRoot = agent.getContainer().get, includeHidden = true)
-        println("Accessible objects: " + accessibleObjects.map(_.name).mkString(", ") )
+        println("Possible objects (with unique referents):" + interface.agentInterface.get.getPossibleObjects().mkString(", "))
         //println("Locations: " + PathFinder.buildLocationGraph(universe) )
 
         //println("Pathfinder test: " + PathFinder.getLocationSequence(universe, startLocation = "living room", endLocation = "foundry")._2.mkString(", "))
