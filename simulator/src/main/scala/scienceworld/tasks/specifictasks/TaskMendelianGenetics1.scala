allenai/ScienceWorld

commit ca6b8c9ac275dbea82a542edf7d175aafbbede6e

Fix typo in location name. green house -> greenhouse

simulator/src/main/scala/scienceworld/tasks/specifictasks/TaskMendelianGenetics1.scala
@@ -29,7 +29,7 @@ import scala.util.control.Breaks.{break, breakable}
 class TaskMendelianGenetics1(val mode:String = MODE_MENDEL_KNOWN) extends TaskParametric {
   val taskName = "task-9-" + mode.replaceAll(" ", "-")
 
-  val locations = Array("green house")
+  val locations = Array("greenhouse")
 
   // Variation 1: Genetic Trait
   val genetics = new ArrayBuffer[ Array[TaskModifier] ]()
@@ -83,7 +83,7 @@ class TaskMendelianGenetics1(val mode:String = MODE_MENDEL_KNOWN) extends TaskPa
 
   // Variation 3: Answer boxes
   val answerBoxes = new ArrayBuffer[ Array[TaskModifier] ]()
-  val answerBoxColors = Array("red", "green", "blue", "orange")
+  val answerBoxColors = Array("red", "green, "blue", "orange")
   for (location <- locations) {
     for (i <- 0 until answerBoxColors.length-1) {
       val colorDom = answerBoxColors(i)
@@ -183,8 +183,8 @@ class TaskMendelianGenetics1(val mode:String = MODE_MENDEL_KNOWN) extends TaskPa
 
       // Moving to helpful locations
       gSequenceUnordered.append(new GoalMoveToNewLocation(_isOptional = true, unlessInLocation = "", description = "move to a new location") )            // Move to any new location
-      gSequenceUnordered.append(new GoalMoveToLocation("green house", _isOptional = true, key = "move1", description = "move to the green house") )
-      gSequenceUnordered.append(new GoalMoveToLocation("green house", _isOptional = true, key = "move2", keysMustBeCompletedBefore = Array("haveSeedJar"), description = "move to the green house (after having seed jar)") )
+      gSequenceUnordered.append(new GoalMoveToLocation("greenhouse", _isOptional = true, key = "move1", description = "move to the greenhouse") )
+      gSequenceUnordered.append(new GoalMoveToLocation("greenhouse", _isOptional = true, key = "move2", keysMustBeCompletedBefore = Array("haveSeedJar"), description = "move to the greenhouse (after having seed jar)") )
 
       // Have soil in flower pots
       var cIdx:Int = 1
@@ -275,7 +275,7 @@ class TaskMendelianGenetics1(val mode:String = MODE_MENDEL_KNOWN) extends TaskPa
     val flowerPotNames = this.getTaskValueStr(modifiers, "flowerPotNames").get.split(",")
     val seedType = this.getTaskValueStr(modifiers, "seedType").get
 
-    val seedLocation = "green house"
+    val seedLocation = "greenhouse"
 
     // Step 1: Move from starting location to task location
     val startLocation = agent.getContainer().get.name
@@ -293,8 +293,8 @@ class TaskMendelianGenetics1(val mode:String = MODE_MENDEL_KNOWN) extends TaskPa
     runAction("pick up seed jar", runner)
 
     /*
-    // Go to green house
-    val (actions1, actionStrs1) = PathFinder.createActionSequence(universe, agent, startLocation = getCurrentAgentLocation(runner).name, endLocation = "green house")
+    // Go to greenhouse
+    val (actions1, actionStrs1) = PathFinder.createActionSequence(universe, agent, startLocation = getCurrentAgentLocation(runner).name, endLocation = "greenhouse")
     runActionSequence(actionStrs1, runner)
      */
 
@@ -363,8 +363,8 @@ class TaskMendelianGenetics1(val mode:String = MODE_MENDEL_KNOWN) extends TaskPa
           // Pick up dirt
           TaskParametric.runAction("pick up soil", runner)
 
-          // Go back to the green house
-          val (actions3, actionStrs3) = PathFinder.createActionSequence(universe, agent, startLocation = getCurrentAgentLocation(runner).name, endLocation = "green house")
+          // Go back to the greenhouse
+          val (actions3, actionStrs3) = PathFinder.createActionSequence(universe, agent, startLocation = getCurrentAgentLocation(runner).name, endLocation = "greenhouse")
           runActionSequence(actionStrs3, runner)
 
           // Move soil to flower pot

simulator/src/main/scala/scienceworld/environments/BuildingMaker.scala
@@ -293,7 +293,7 @@ object RoomMaker {
    */
   def mkGreenhouse(sewer:EnvObject): Room = {
     // House
-    val room = new Room("green house")
+    val room = new Room("greenhouse")
 
 
     // Water jug
@@ -513,7 +513,7 @@ object BuildingMaker {
     val roomWorkshop = RoomMaker.mkWorkshop()
     universe.addObject(roomWorkshop)
 
-    // Green House
+    // Greenhouse
     val roomGreenhouse = RoomMaker.mkGreenhouse(sewer)
     universe.addObject(roomGreenhouse)
 
@@ -536,7 +536,7 @@ object BuildingMaker {
     val foundry = RoomMaker.mkFoundry(sewer)
     universe.addObject(foundry)
 
-    // Randomly put a shoven in either the green house or outside
+    // Randomly put a shoven in either the greenhouse or outside
     // Shovel
     val shovel = new Shovel()
     if (Random.nextInt(2) == 1) {

simulator/src/main/scala/scienceworld/environments/EnvironmentMaker.scala
@@ -46,7 +46,7 @@ object EnvironmentMaker {
     //## Specific start point in environment
     //for (location <- locations) if (location.name == "workshop") location.addObject(agent)
     //for (location <- locations) if (location.name == "kitchen") location.addObject(agent)
-    //for (location <- locations) if (location.name == "green house") location.addObject(agent)
+    //for (location <- locations) if (location.name == "greenhouse") location.addObject(agent)
     //for (location <- locations) if (location.name == "outside") location.addObject(agent)
     //for (location <- locations) if (location.name == "art studio") location.addObject(agent)
     //for (location <- locations) if (location.name == "hallway") location.addObject(agent)

simulator/src/main/scala/scienceworld/tasks/specifictasks/TaskChemistryMixPaint.scala
@@ -29,7 +29,7 @@ class TaskChemistryMixPaint(val mode:String = MODE_CHEMISTRY_MIX_PAINT_SECONDARY
   val mixingContainers = Random.shuffle( List(new GlassCup(), new WoodBowl(), new Jug()) )
   val locations = Array("art studio", "workshop")
 
-  //val locations = Array("green house")
+  //val locations = Array("greenhouse")
   for (location <- locations) {
     val paints2 = Array(new RedPaint, new BluePaint, new YellowPaint) //, new GreenPaint, new VioletPaint, new OrangePaint)
 

simulator/src/main/scala/scienceworld/tasks/specifictasks/TaskElectricCircuit.scala
@@ -453,12 +453,12 @@ class TaskElectricCircuit(val mode:String = MODE_POWER_COMPONENT) extends TaskPa
 
       var livingThingLocation = "outside"
       if (mode == MODE_LIVING) {
-        val shuffledLocations = Random.shuffle(List("outside", "green house"))
+        val shuffledLocations = Random.shuffle(List("outside", "greenhouse"))
         livingThingLocation = shuffledLocations(0)
       } else if (mode == MODE_ANIMAL) {
         livingThingLocation = "outside"         // Animals are outside
       } else if (mode == MODE_PLANT) {
-        livingThingLocation = "green house"     // Plants are in the green house
+        livingThingLocation = "greenhouse"     // Plants are in the greenhouse
       }
 
       // Step 1: Move from starting location to a place likely to have animals

simulator/src/main/scala/scienceworld/tasks/specifictasks/TaskFindLivingNonLiving.scala
@@ -31,7 +31,7 @@ class TaskFindLivingNonLiving(val mode:String = MODE_LIVING) extends TaskParamet
   val livingThings = new ArrayBuffer[ Array[TaskModifier] ]()
   for (i <- 0 until 10) {
     val livingThingsToAdd = TaskFindLivingNonLiving.mkRandomAnimals(location = "outside", numAnimals = 3, variationIdx = i) ++
-                            TaskFindLivingNonLiving.mkRandomPlants(location = "green house", numPlants = 3, variationIdx = i)
+                            TaskFindLivingNonLiving.mkRandomPlants(location = "greenhouse", numPlants = 3, variationIdx = i)
     livingThings.append( livingThingsToAdd )
   }
 
@@ -249,12 +249,12 @@ class TaskFindLivingNonLiving(val mode:String = MODE_LIVING) extends TaskParamet
 
     var livingThingLocation = "outside"
     if (mode == MODE_LIVING) {
-      val shuffledLocations = Random.shuffle(List("outside", "green house"))
+      val shuffledLocations = Random.shuffle(List("outside", "greenhouse"))
       livingThingLocation = shuffledLocations(0)
     } else if (mode == MODE_ANIMAL) {
       livingThingLocation = "outside"         // Animals are outside
     } else if (mode == MODE_PLANT) {
-      livingThingLocation = "green house"     // Plants are in the green house
+      livingThingLocation = "greenhouse"     // Plants are in the greenhouse
     }
 
     // Step 1: Move from starting location to a place likely to have animals

simulator/src/main/scala/scienceworld/tasks/specifictasks/TaskGrowPlant.scala
@@ -38,8 +38,8 @@ class TaskGrowPlant(val mode:String = MODE_LIVING) extends TaskParametric {
 
   // Variation 1: Which seeds to grow
   val seeds = new ArrayBuffer[ Array[TaskModifier] ]()
-  val locations = Array("kitchen", "bathroom", "living room", "bedroom", "workshop", "green house")
-  //val locations = Array("green house")
+  val locations = Array("kitchen", "bathroom", "living room", "bedroom", "workshop", "greenhouse")
+  //val locations = Array("greenhouse")
   for (location <- locations) {
 
     val seedJarApple = TaskGrowPlant.mkSeedJar(PlantReproduction.PLANT_APPLE)
@@ -80,7 +80,7 @@ class TaskGrowPlant(val mode:String = MODE_LIVING) extends TaskParametric {
     val container = new FlowerPot()
     container.name = "flower pot " + (i+1)
     container.addObject( new Soil() )
-    case1tm.append( new TaskObject(container.name, Some(container), roomToGenerateIn = "green house", Array.empty[String], generateNear = 0))
+    case1tm.append( new TaskObject(container.name, Some(container), roomToGenerateIn = "greenhouse", Array.empty[String], generateNear = 0))
     containerNames1.append(container.name)
   }
   case1tm.append( new TaskValueStr("containerNames", containerNames1.mkString(",")))
@@ -93,11 +93,11 @@ class TaskGrowPlant(val mode:String = MODE_LIVING) extends TaskParametric {
   for (i <- 0 until numContainers) {
     val container = new FlowerPot()
     container.name = "flower pot " + (i+1)
-    case2tm.append( new TaskObject(container.name, Some(container), roomToGenerateIn = "green house", Array.empty[String], generateNear = 0))
+    case2tm.append( new TaskObject(container.name, Some(container), roomToGenerateIn = "greenhouse", Array.empty[String], generateNear = 0))
     containerNames2.append(container.name)
 
     val soil = new Soil()
-    case2tm.append( new TaskObject(soil.name, Some(soil), roomToGenerateIn = "green house", Array.empty[String], generateNear = 0))
+    case2tm.append( new TaskObject(soil.name, Some(soil), roomToGenerateIn = "greenhouse", Array.empty[String], generateNear = 0))
   }
   case2tm.append( new TaskValueStr("containerNames", containerNames2.mkString(",")))
   plantContainers.append( case2tm.toArray )
@@ -108,7 +108,7 @@ class TaskGrowPlant(val mode:String = MODE_LIVING) extends TaskParametric {
   for (i <- 0 until numContainers) {
     val container = new FlowerPot()
     container.name = "flower pot " + (i+1)
-    case3tm.append( new TaskObject(container.name, Some(container), roomToGenerateIn = "green house", Array.empty[String], generateNear = 0))
+    case3tm.append( new TaskObject(container.name, Some(container), roomToGenerateIn = "greenhouse", Array.empty[String], generateNear = 0))
     containerNames3.append(container.name)
   }
   case3tm.append( new TaskValueStr("containerNames", containerNames3.mkString(",")))
@@ -192,8 +192,8 @@ class TaskGrowPlant(val mode:String = MODE_LIVING) extends TaskParametric {
 
       // Moving to helpful locations
       gSequenceUnordered.append(new GoalMoveToNewLocation(_isOptional = true, unlessInLocation = "", description = "move to a new location") )            // Move to any new location
-      gSequenceUnordered.append(new GoalMoveToLocation("green house", _isOptional = true, key = "move1", description = "move to the green house") )
-      gSequenceUnordered.append(new GoalMoveToLocation("green house", _isOptional = true, key = "move2", keysMustBeCompletedBefore = Array("haveSeedJar"), description = "move to the green house (after having seed jar)") )
+      gSequenceUnordered.append(new GoalMoveToLocation("greenhouse", _isOptional = true, key = "move1", description = "move to the greenhouse") )
+      gSequenceUnordered.append(new GoalMoveToLocation("greenhouse", _isOptional = true, key = "move2", keysMustBeCompletedBefore = Array("haveSeedJar"), description = "move to the greenhouse (after having seed jar)") )
 
       // Have soil in flower pots
       var cIdx:Int = 1
@@ -235,8 +235,8 @@ class TaskGrowPlant(val mode:String = MODE_LIVING) extends TaskParametric {
 
       // Moving to helpful locations
       gSequenceUnordered.append(new GoalMoveToNewLocation(_isOptional = true, unlessInLocation = "", description = "move to a new location") )            // Move to any new location
-      gSequenceUnordered.append(new GoalMoveToLocation("green house", _isOptional = true, key = "move1", description = "move to the green house") )
-      gSequenceUnordered.append(new GoalMoveToLocation("green house", _isOptional = true, key = "move2", keysMustBeCompletedBefore = Array("haveSeedJar"), description = "move to the green house (after having seed jar)") )
+      gSequenceUnordered.append(new GoalMoveToLocation("greenhouse", _isOptional = true, key = "move1", description = "move to the greenhouse") )
+      gSequenceUnordered.append(new GoalMoveToLocation("greenhouse", _isOptional = true, key = "move2", keysMustBeCompletedBefore = Array("haveSeedJar"), description = "move to the greenhouse (after having seed jar)") )
 
       // Have soil in flower pots
       var cIdx:Int = 1
@@ -336,8 +336,8 @@ class TaskGrowPlant(val mode:String = MODE_LIVING) extends TaskParametric {
     //runAction("pick up " + PathFinder.getObjUniqueReferent(seedJar, getCurrentAgentLocation(runner)).get, runner)
     runAction("pick up seed jar", runner)
 
-    // Go to green house
-    val (actions1, actionStrs1) = PathFinder.createActionSequence(universe, agent, startLocation = getCurrentAgentLocation(runner).name, endLocation = "green house")
+    // Go to greenhouse
+    val (actions1, actionStrs1) = PathFinder.createActionSequence(universe, agent, startLocation = getCurrentAgentLocation(runner).name, endLocation = "greenhouse")
     runActionSequence(actionStrs1, runner)
 
     // Look around
@@ -406,8 +406,8 @@ class TaskGrowPlant(val mode:String = MODE_LIVING) extends TaskParametric {
           // Pick up dirt
           TaskParametric.runAction("pick up soil", runner)
 
-          // Go back to the green house
-          val (actions3, actionStrs3) = PathFinder.createActionSequence(universe, agent, startLocation = getCurrentAgentLocation(runner).name, endLocation = "green house")
+          // Go back to the greenhouse
+          val (actions3, actionStrs3) = PathFinder.createActionSequence(universe, agent, startLocation = getCurrentAgentLocation(runner).name, endLocation = "greenhouse")
           runActionSequence(actionStrs3, runner)
 
           // Move soil to flower pot
@@ -569,8 +569,8 @@ class TaskGrowPlant(val mode:String = MODE_LIVING) extends TaskParametric {
     //runAction("pick up " + PathFinder.getObjUniqueReferent(seedJar, getCurrentAgentLocation(runner)).get, runner)
     runAction("pick up seed jar", runner)
 
-    // Go to green house
-    val (actions1, actionStrs1) = PathFinder.createActionSequence(universe, agent, startLocation = getCurrentAgentLocation(runner).name, endLocation = "green house")
+    // Go to greenhouse
+    val (actions1, actionStrs1) = PathFinder.createActionSequence(universe, agent, startLocation = getCurrentAgentLocation(runner).name, endLocation = "greenhouse")
     runActionSequence(actionStrs1, runner)
 
     // Look around
@@ -634,8 +634,8 @@ class TaskGrowPlant(val mode:String = MODE_LIVING) extends TaskParametric {
         // Pick up dirt
         TaskParametric.runAction("pick up soil", runner)
 
-        // Go back to the green house
-        val (actions3, actionStrs3) = PathFinder.createActionSequence(universe, agent, startLocation = getCurrentAgentLocation(runner).name, endLocation = "green house")
+        // Go back to the greenhouse
+        val (actions3, actionStrs3) = PathFinder.createActionSequence(universe, agent, startLocation = getCurrentAgentLocation(runner).name, endLocation = "greenhouse")
         runActionSequence(actionStrs3, runner)
 
         // Move soil to flower pot

simulator/src/main/scala/scienceworld/tasks/specifictasks/TaskMendelianGenetics2.scala
@@ -32,7 +32,7 @@ class TaskMendelianGenetics2 {
 class TaskMendelialGenetics2(val mode:String = MODE_MENDEL_UNKNOWN) extends TaskParametric {
   val taskName = "task-9-" + mode.replaceAll(" ", "-")
 
-  val locations = Array("green house")
+  val locations = Array("greenhouse")
 
   // Variation 1: Genetic Trait
   val genetics = new ArrayBuffer[ Array[TaskModifier] ]()
@@ -223,8 +223,8 @@ class TaskMendelialGenetics2(val mode:String = MODE_MENDEL_UNKNOWN) extends Task
 
       // Moving to helpful locations
       gSequenceUnordered.append(new GoalMoveToNewLocation(_isOptional = true, unlessInLocation = "", description = "move to a new location") )            // Move to any new location
-      gSequenceUnordered.append(new GoalMoveToLocation("green house", _isOptional = true, key = "move1", description = "move to the green house") )
-      gSequenceUnordered.append(new GoalMoveToLocation("green house", _isOptional = true, key = "move2", keysMustBeCompletedBefore = Array("haveSeedJar"), description = "move to the green house (after having seed jar)") )
+      gSequenceUnordered.append(new GoalMoveToLocation("greenhouse", _isOptional = true, key = "move1", description = "move to the greenhouse") )
+      gSequenceUnordered.append(new GoalMoveToLocation("greenhouse", _isOptional = true, key = "move2", keysMustBeCompletedBefore = Array("haveSeedJar"), description = "move to the greenhouse (after having seed jar)") )
 
       // Have soil in flower pots
       var cIdx:Int = 1
@@ -315,7 +315,7 @@ class TaskMendelialGenetics2(val mode:String = MODE_MENDEL_UNKNOWN) extends Task
     val flowerPotNames = this.getTaskValueStr(modifiers, "flowerPotNames").get.split(",")
     val seedType = this.getTaskValueStr(modifiers, "seedType").get
 
-    val seedLocation = "green house"
+    val seedLocation = "greenhouse"
 
     // Step 1: Move from starting location to task location
     val startLocation = agent.getContainer().get.name
@@ -333,8 +333,8 @@ class TaskMendelialGenetics2(val mode:String = MODE_MENDEL_UNKNOWN) extends Task
     runAction("pick up seed jar", runner)
 
     /*
-    // Go to green house
-    val (actions1, actionStrs1) = PathFinder.createActionSequence(universe, agent, startLocation = getCurrentAgentLocation(runner).name, endLocation = "green house")
+    // Go to greenhouse
+    val (actions1, actionStrs1) = PathFinder.createActionSequence(universe, agent, startLocation = getCurrentAgentLocation(runner).name, endLocation = "greenhouse")
     runActionSequence(actionStrs1, runner)
      */
 
@@ -403,8 +403,8 @@ class TaskMendelialGenetics2(val mode:String = MODE_MENDEL_UNKNOWN) extends Task
           // Pick up dirt
           TaskParametric.runAction("pick up soil", runner)
 
-          // Go back to the green house
-          val (actions3, actionStrs3) = PathFinder.createActionSequence(universe, agent, startLocation = getCurrentAgentLocation(runner).name, endLocation = "green house")
+          // Go back to the greenhouse
+          val (actions3, actionStrs3) = PathFinder.createActionSequence(universe, agent, startLocation = getCurrentAgentLocation(runner).name, endLocation = "greenhouse")
           runActionSequence(actionStrs3, runner)
 
           // Move soil to flower pot

commit cf4ce514e37c91ee7ab5001627d9d8ec59b2dd6b

Fix syntax error introduced in ca6b8c9ac275dbea82a542edf7d175aafbbede6e

simulator/src/main/scala/scienceworld/tasks/specifictasks/TaskMendelianGenetics1.scala
@@ -83,7 +83,7 @@ class TaskMendelianGenetics1(val mode:String = MODE_MENDEL_KNOWN) extends TaskPa
 
   // Variation 3: Answer boxes
   val answerBoxes = new ArrayBuffer[ Array[TaskModifier] ]()
-  val answerBoxColors = Array("red", "green, "blue", "orange")
+  val answerBoxColors = Array("red", "green", "blue", "orange")
   for (location <- locations) {
     for (i <- 0 until answerBoxColors.length-1) {
       val colorDom = answerBoxColors(i)