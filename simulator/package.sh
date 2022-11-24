59993fa82106839115d956c8b781ec6599394ea7
Marc-Alexandre Côté <marc.cote.19@gmail.com>
2022-11-23 20:22:53 -0500

    Update JAR

--- simulator/package.sh
+++ simulator/package.sh
@@ -2,6 +2,10 @@
 
 set -euo pipefail
 
+pushd "$(dirname "$(readlink -f "$0")")"
+
 sbt assembly
 version=`cat build.sbt | grep version | cut -d '"' -f2`
 cp target/scala-2.12/scienceworld-scala-assembly-${version}.jar ../scienceworld/scienceworld-${version}.jar
+
+popd
