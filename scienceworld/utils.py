e5a01d25268aa4cf8b240f0812b8c8dacac802c4
Andrew Kaminer <akaminer@gatech.edu>
2023-12-14 13:24:01 -0500

    Add deprecated_api_warning in utils for putting up a warning when a deprecated api feature is used

--- scienceworld/utils.py
+++ scienceworld/utils.py
@@ -1,4 +1,5 @@
 import re
+import warnings
 
 from scienceworld.constants import NAME2ID, ID2TASK
 
@@ -17,3 +18,25 @@ def infer_task(name_or_id):
     name_or_id = re.sub(r"task-(\d|a|b)+-|[()]", "", name_or_id)
 
     return name_or_id
+
+
+def deprecated_api_warning(pending=True, camel_case=True):
+    if pending:
+        depstatus = "This feature will be deprecated soon."
+    else:
+        depstatus = "This feature is deprecated."
+
+    if snake_case:
+        message = f"You are using the camel case naming convention for the \
+                python API. {depstatus} Please use snake case instead."
+    else:
+        message = f"{depstatus}. Please do not use it, as it may lead to \
+                unexpected behavior."
+
+    if pending:
+        category = PendingDeprecationWarning
+    else:
+        category = DeprecationWarning
+
+
+    warnings.warn(message, category, stacklevel=2)
